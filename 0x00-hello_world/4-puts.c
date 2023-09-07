afb4637dd998714309031754dabf5d569ec896bd
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2023-09-07 16:15:11 -0700

    Programming is like building a multilingual puzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+  * using char function
+  *
+  * Return: always 0 (Successs)
+  */
+  
+int main(void)
+{
+	char my_str[] = ""Programming is like building a multilingual puzzle";
+
+	puts(my_str);
+
+	return 0;
+
+}
