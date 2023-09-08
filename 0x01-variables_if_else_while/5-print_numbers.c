fef8ec53628b990e9e0a75d8c75152c3517cb55e
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2023-09-08 16:35:58 -0700

    Base 10 from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print digits of base 10
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+
+	putchar(i + '0');
+	putchar('\n');
+	return (0);
+}
