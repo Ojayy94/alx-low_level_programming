24be654f4e33c57aaac3c339c9d4408af60bc96e
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2023-09-07 19:12:44 -0700

    Making sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,7 +15,7 @@ int main(void)
 	long int l;
 	long long int j;
 	float f;
-	
+
 	printf("Size of a char: %lu byte(s)\n", (unsigned long) sizeof(c));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long) sizeof(i));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long) sizeof(l));
