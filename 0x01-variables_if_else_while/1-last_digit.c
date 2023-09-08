844e75682c2389ef0a80cfb2b32a565079985a28
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2023-09-08 11:34:27 -0700

    assign a andom number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,24 +10,24 @@
 
 int main(void)
 {
-	int n;
+	int n, numb;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 5)
 	{
-		printf("Last digit of %d is 6 and is greater than 5\n", n);
+		printf("Last digit of %d is %d and is greater than 5\n", n, numb);
 	}
 
 	else if (n == 0)
 	{
-		printf("Last digit of %d is 0 and is 0\n", n);
+		printf("Last digit of %d is %d and is 0\n", n, numb);
 	}
 
-	else if (n < 6 && n != 0)
+	else if (n < 6 && numb != 0)
 	{
-		printf("Last digit of %d is 5 and is less than 6 and not 0\n", n);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, numb);
 	}
 	return (0);
 }
