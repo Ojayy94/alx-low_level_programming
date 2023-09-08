4e64064f8673af5f1381ef9683d66b810e20d275
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2023-09-07 18:40:40 -0700

    Making sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Printing with sizes
+ *
+ * Return: Always 0 (Successs)
+ */
+
+int main (void)
+{
+	char c;
+	int i;
+	long int l;
+	long long int j;
+	float f;
+	
+	printf("Size of a char: %lu.\n", (unsigned long) size of (c));
+	printf("Size of an int: %lu.\n", (unsigned long) size of (i));
+	printf("Size of a long int: %lu.\n", (unsigned long) size of (l));
+	printf("Size of a long long int: %lu.\n", (unsigned long) size of (j));
+	printf("Size of a float: %lu.\n", (unsigned long) size of (f));
+	return (0);
+}
