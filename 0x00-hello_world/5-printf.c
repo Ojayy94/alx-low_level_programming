495d0eeb38fc334289c6de0a7b602142099db92f
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2023-09-07 17:46:58 -0700

    Using printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 
 /**
- * Main - Entry point
- *
- * Description: Using printf function printf()
+ * main - Using printf function printf
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
-
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
