5f18b2b3ce804ab4722b9184a8064c4e4ecabfef
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2023-09-08 17:13:39 -0700

    alphabet reversed

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print abc as cab
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char a = 'z';
+
+	for (a = 'z'; a >= 'a'; --a)
+
+	putchar(a);
+	putchar('\n');
+	return (0);
+}
