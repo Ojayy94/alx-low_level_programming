a402fb76c26228f0e443ef0ed3cf79275d95aa10
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2023-09-09 15:17:42 -0700

    possible combination of 2-digit 1-9

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,20 +10,21 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = i + 1; j < 10; j++)
+		for (j = i + 1; j <= 9; j++)
 		{
 			putchar((i % 10) + '0');
 			putchar((j % 10) + '0');
 
-			if (i == 8 && j == 10)
+			if (i == 8 && j == 9)
 				continue;
 
 			putchar(',');
 			putchar(' ');
 		}
 	}
+
 	putchar('\n');
 	return (0);
 }
