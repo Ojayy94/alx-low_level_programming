ce37caadec09bd14a65def60191c0d33d834e315
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2023-09-12 17:54:29 -0700

    sum of two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - sum of two integers
+ *
+ * @n1: first number
+ * @n2: second number
+ *
+ * Return: 0 (Success)
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
