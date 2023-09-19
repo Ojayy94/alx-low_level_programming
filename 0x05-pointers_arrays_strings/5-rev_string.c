cff6930d8cba5ac0ec91fa51b9583277f1343897
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2023-09-19 15:33:13 -0700

    reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,11 +1,24 @@
 #include "main.h"
 
 /**
-* _puts - to print a string and a new line
+* rev_string - to print a reverse string
 * @s: Use case
 * Return: Success
 */
 
 void rev_string(char *s)
 {
+	char rev = s[0];
+	int count = 0;
+	int a;
+
+	while (s[count] != '\0')
+		count++;
+	for (a = 0; a < count; a++)
+	{
+		count--;
+		rev = s[a];
+		s[a] = s[count];
+		s[count] = rev;
+	}
 }
