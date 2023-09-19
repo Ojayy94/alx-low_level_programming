ce52cc2cfaedb0a3165ac954e877c61396dff3d0
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2023-09-19 14:12:44 -0700

    print reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_rev - to print a string and a new line
+* @s: Use case
+* Return: Success
+*/
+
+void print_rev(char *s)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0'; a++)
+;
+	for (a = a - 1; s[a] != '\0'; a--)
+	{
+		_putchar(s[a]);
+	}
+	_putchar('\n');
+}
