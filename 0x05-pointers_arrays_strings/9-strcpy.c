79950a25d6936ba381ef12b5e10b90347f0dc8e8
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2023-09-20 02:46:53 -0700

    src as a pointer

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,10 +11,10 @@ char *_strcpy(char *dest, char *src)
 {
 	int a = 0;
 
-	while (src[a])
+	for (a = 0; src[a] != '\0'; a++)
 	{
 		dest[a] = src[a];
-		a++;
 	}
+	dest[a] = '\0';
 	return (dest);
 }
