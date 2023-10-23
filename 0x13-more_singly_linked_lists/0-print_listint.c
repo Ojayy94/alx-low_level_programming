97298de419ebd75f39c9001c6c018e99d3c9d026
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2023-10-23 08:01:55 -0700

    print

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_listint - function that prints all elements of list
+ * @h: const h
+ * Return: nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t nodes = 0;
+
+	for (nodes = 0; nodes++; h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (nodes);
+}
