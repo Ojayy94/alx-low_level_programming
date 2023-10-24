5ae5d45a03076d47c6235d866add497652ec7f85
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2023-10-24 05:22:09 -0700

    Program to add all node in a linked list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * sum_listint - Program that adds node
+ * @head: Node head to sum
+ * Return: Sum
+ */
+
+int sum_listint(listint_t *head);
+{
+	int add = 0;
+
+	while (head)
+	{
+		add = add + head->next;
+		head = head->next;
+	}
+	return (add);
+}
