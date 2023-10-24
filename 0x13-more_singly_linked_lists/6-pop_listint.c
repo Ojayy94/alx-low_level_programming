26aee9b4e60bb23de25614661009d5d097f4eae9
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2023-10-24 03:28:36 -0700

    Pop head node

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -11,12 +11,13 @@ int pop_listint(listint_t **head)
 	listint_t *node;
 	int a;
 
-	if (head == NULL)
+	if (*head == NULL)
 		return (0);
 
 	node = *head;
 	a = (*head)->n;
 	*head = (*head)->next;
+	
 	free(node);
 
 	return (a);
