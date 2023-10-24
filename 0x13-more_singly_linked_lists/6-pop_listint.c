dcabd2b7183b00b588a8325006d5a666e4cd5f08
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2023-10-24 03:17:26 -0700

    Pop head node

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * pop_listint - Program to pop a linked list
+ * @head: Node head
+ * Return: Pop node
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *node;
+	int a;
+
+	if (head == NULL)
+		return (0);
+
+	*node = (*head)->next;
+	a = (*head)->n
+	*head = (*head)->next;
+	free(node);
+
+	return (a);
+}
