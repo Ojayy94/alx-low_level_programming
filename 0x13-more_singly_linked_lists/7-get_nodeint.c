7bfa5b9e640e2fadb87e49c0da4185a01bb71508
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2023-10-24 05:09:05 -0700

    Add to node at index

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - Function to get index node
+ * @head: Node head
+ * @index: node index to return
+ * Return: index
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t node;
+
+	for (node = 0; node < index; node++)
+	{
+		if (head != NULL)
+			head = head->next;
+	}
+	return (head);
+}
