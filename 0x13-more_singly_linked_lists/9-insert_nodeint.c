1c85fd9e1452d0e85cc289086aa9467f6f13db9a
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2023-10-24 06:43:00 -0700

    Add to a node at index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -34,7 +34,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			return (NULL);
 		else if (step->next == NULL)
 			return (NULL);
-		
+
 		step = step->next;
 	}
 	node->next = step->next;
