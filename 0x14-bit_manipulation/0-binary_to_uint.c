14a273490c1c9c50357afa431bd2f793aa404dc7
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2023-10-26 03:28:26 -0700

    function that prints the binary representation of a number

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * binary_to_uint - program to convert base to unsigned int
+ * @b: Base number
+ * Return: Converted number
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num = 0;
+	unsigned int i;
+
+	size_t len = strlen(b);
+
+	if (b == NULL)
+		return (0);
+
+	for (i = 0; i < len; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		num = (num << 1) | (b[i] - '0');
+	}
+	return (num);
+}
