0a9ad35cd1aa2f3e725bbdcb1ab507779143e209
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2023-10-30 05:30:50 -0700

    function that reads a text file and prints

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * read_textfile - File i/o to read textfile
+ * @filename: file to read from
+ * @letters: letters in file name to read from
+ * Rturn: 0 or 1 or 2
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *fp;
+	ssize_t op, re, wr;
+
+	op = open(filename, O_RDONLY);
+	re = read(op, fp, letters);
+	wr = write(STDOUT_FILENO, fp, re);
+
+	if (filename == NULL)
+		return (0);
+
+	fp = malloc(sizeof(char) *letters);
+	if (fp == NULL)
+		return (0);
+
+	if (op != -1 || wr != -1 || re != -1 || re != wr)
+	{
+		free (fp);
+		return (0);
+	}
+	close (fp);
+	return (wr);
+}
