3dfe6d32902bc690d3014ce06db17c6144fd3859
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2023-12-07 07:33:57 -0800

    add note at beginning

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - Ad node at beginning of list
+ * @head: Head node
+ * @n: add position
+ * Return: Null if failed and 1 while true
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
+
+	new_node = malloc(sizeof(dlistint_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = *head;
+
+	if (*head != NULL)
+		(*head)->prev = new_node;
+	*head = new_node;
+
+	return (new_node);
+}
