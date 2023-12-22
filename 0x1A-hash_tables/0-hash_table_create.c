0aade8a7a78e2f8c8c8a6f1652d7b709c8bf34b3
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2023-12-22 08:58:03 -0800

    create hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+  * hash_table_create - Function that creates a hash table
+  * @size: Size of the array
+  * Return: A pointer to the newly created hash table
+  */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int i;
+	hash_table_t *ht = NULL;
+	hash_node_t **a = NULL;
+
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
+
+	a = malloc(sizeof(hash_node_t *) * size);
+	if (a == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; ++i)
+		a[i] = NULL;
+
+	ht->size = size;
+	ht->array = a;
+
+	return (ht);
+}
