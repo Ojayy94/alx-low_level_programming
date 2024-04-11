4387fd281e625a19e05158ba3766493d5f592c94
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2024-04-11 12:16:01 -0700

    binary search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -17,7 +17,7 @@ int binary_search(int *array, size_t size, int value)
 	if (!array)
 		return (-1);
 
-	for (left = 0; right = size - 1; right >= left;)
+	for (left = 0, right = size - 1; right >= left;)
 		printf('Searching in array: ');
 		for (i = left; i < right; i++)
 			printf("%d, ", array[i]);
